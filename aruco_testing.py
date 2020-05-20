2c00f1bfb39090f86affea2f97f37914c5b7699b
Farhan Faruqui <farhanfaruqui13@gmail.com>
2020-05-20 16:09:24 -0400

    Comments

--- aruco_testing.py
+++ aruco_testing.py
@@ -1,6 +1,7 @@
 import cv2
 import cv2.aruco as aruco
 
+# Initializing capture device
 cap = cv2.VideoCapture(0)
 parameters = aruco.DetectorParameters_create()
 markerDictionary = aruco.Dictionary_get(aruco.DICT_6X6_1000)
@@ -10,14 +11,20 @@ while True:
     boolAndFrame = cap.read()
     ret = boolAndFrame[0]
     frame = boolAndFrame[1]
+    # Retrieving grayscale image
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     corners, ids, _ = aruco.detectMarkers(gray, markerDictionary, parameters=parameters)
+    # Actual detection of the markers
     aruco.drawDetectedMarkers(frame, corners, ids)
+
+    # Showing the detected markers along with flipping the view along the y-axis
     cv2.imshow('Detecting markers', cv2.flip(frame, 1))
 
     # if ids is not None:
     #   print(ids)
+
+    # Press q to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
